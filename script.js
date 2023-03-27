3e902da7f3fc02c315ee2568400aca651c8d460e
Roni Wellington <roniwellington@gmail.com>
2023-03-27 20:48:16 -0300

    botão

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 const btnHam = document.querySelector('.ham-btn');
 const btnTimes = document.querySelector('.times-btn');
@@ -21,3 +21,22 @@ btnTimes.addEventListener('click', function(){
 })
 
 
+
+/**
+ * header sticky & go to top
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
+});
